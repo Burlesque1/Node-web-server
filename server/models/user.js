1ea308e8137ad515a7e07f48bd72027910e026cb
Burlesque1 <123176000@qq.com>
2018-03-19 16:37:16 -0400

    8-4

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,71 @@
 const mongoose = require('mongoose');
-const validator = require('validator')
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email',
+      isAsync: true // the original method has been depreciated
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
+});
+
+// cannot use arrow function here
+// because arrow function dose not bind to "this"
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  // user.tokens.push({access, token});
+  user.tokens = user.tokens.concat([{access, token}]); // across different version of Mongoose
+
+  return user.save().then(() => {
+    return token;
+  });
+  // make this chain inside server.js so here just use return
+  // user.save().then(() => {
+  //   return token;
+  // }).then((token) => {
+  // });
+};
+
+// cannot add method directly to model
+var User = mongoose.model('User', UserSchema);
+
+module.exports = {User}
+
+/* var User = mongoose.model('User', {
   email: {
     type: String,
     required: true,
@@ -30,4 +94,4 @@ var User = mongoose.model('User', {
   }]
 });
 
-module.exports = {User}
+module.exports = {User} */
