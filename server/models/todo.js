a7e03b08b5eee171ca481235f4e17afeeff40965
Burlesque1 <123176000@qq.com>
2018-03-19 15:13:44 -0400

    init

---
+++ server/models/todo.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+// this is the collection name
+var Todo = mongoose.model('Todos', { // only lower case in mongoose?
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+module.exports = {Todo};
