52c7be506b9fc2011553b5d4feda63c94b41d77a
Burlesque1 <123176000@qq.com>
2018-03-30 16:02:19 -0400

    hello world

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
-console.log(process.env)
+// for test purpose
+require('./config/config')
+
+
+const express = require('express');
+
+
+
+
+var app = express();
+
+const port = process.env.PORT;
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => {
+    console.log(`Started up at port ${port}`);
+})
+
+module.exports = {app};
