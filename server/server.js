1f3f2c0d883359778c1888d7be06ccc8a5c3db65
Burlesque1 <123176000@qq.com>
2018-03-30 17:37:48 -0400

    add hbs

--- server/server.js
+++ server/server.js
@@ -5,23 +5,43 @@ require('./config/config')
 const express = require('express');
 const hbs = require('hbs')
 
-
-
+const port = process.env.PORT;
 
 var app = express();
+console.log(__dirname)
+hbs.registerPartials('C:\\Users\\Omelet\\Desktop\\Node\\R8\\views\\partials');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+app.set('view engine', 'hbs');
 
-const port = process.env.PORT;
+
+// views should be put in the same dir with node_modules
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+// static serves static files and uses complete path
+// public should include pages except index.html
+app.use(express.static(__dirname + '/public'))
 
 app.use((req, res, next) => {
     var now = new Date().toString();
     console.log(`${now} ${req.method} ${req.url}`);
     next();
-})
+});
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs')})
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+      pageTitle: 'Home Page',
+      welcomeMessage: 'Welcome to my website'
+    });
+});
 
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
