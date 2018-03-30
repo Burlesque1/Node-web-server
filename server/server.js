6030d1610eb7946001031669861a7f27aea9a8d8
Burlesque1 <123176000@qq.com>
2018-03-30 16:26:50 -0400

    add views

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('./config/config')
 
 
 const express = require('express');
+const hbs = require('hbs')
 
 
 
@@ -10,6 +11,15 @@ const express = require('express');
 var app = express();
 
 const port = process.env.PORT;
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    console.log(`${now} ${req.method} ${req.url}`);
+    next();
+})
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs')})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
