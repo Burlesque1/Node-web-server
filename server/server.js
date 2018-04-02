de6784da0abef22c7dd5b41c598fda2488271468
Burlesque1 <123176000@qq.com>
2018-04-02 17:09:45 -0400

    add login function

--- server/server.js
+++ server/server.js
@@ -3,13 +3,23 @@ require('./config/config')
 
 
 const express = require('express');
-const hbs = require('hbs')
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const moment = require('moment');
+const _ = require('lodash');
+const validator = require('validator');
+
+var {mongoose} = require('./db/mongoose');
+var {authenticate} = require('./middleware/authenticate');
+var {User} = require('./models/user');
 
 const port = process.env.PORT;
 
 var app = express();
-console.log(__dirname)
+
+// --------------------------------------------------------------------------???
 hbs.registerPartials('C:\\Users\\Omelet\\Desktop\\Node\\R8\\views\\partials');
+// --------------------------------------------------------------------------???
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
 });
@@ -27,12 +37,12 @@ app.set('view engine', 'hbs');
 // static serves static files and uses complete path
 // public should include pages except index.html
 app.use(express.static(__dirname + '/public'))
-
-app.use((req, res, next) => {
-    var now = new Date().toString();
-    console.log(`${now} ${req.method} ${req.url}`);
-    next();
-});
+app.use(bodyParser.urlencoded({extended: true}));
+// app.use((req, res, next) => {
+//     var now = new Date().toString();
+//     console.log(`${now} ${req.method} ${req.url}`);
+//     next();
+// });
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
@@ -41,12 +51,27 @@ app.get('/', (req, res) => {
     });
 });
 
-app.post('/user', (req, res) => {
-    console.log(req);
-    res.render('loginpage.hbs');
+app.post('/room', async (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    console.log(body, typeof body)
+    try{
+        if(!validator.isEmail(body.email) || body.password.length < 6){
+            
+            // pop up here
+
+            throw new Error('not email or password too short');      
+        }
+        var user = await User.findByCredentials(body.email, body.password);
+        var token = await user.generateAuthToken();
+        // res.header('x-auth', token).send(user); // ??????????????
+        res.render('room.hbs');
+    } catch(e){
+        console.log(e.message);
+        res.status(400).redirect('/');
+    }
 })
 app.listen(port, () => {
-    console.log(`Started up at port ${port}`);
+    console.log(`Started up at port ${port} at ${moment()}`);
 })
 
 module.exports = {app};
