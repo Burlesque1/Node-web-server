e9a1d6f6ce0379dff4c5ee86dce7e633f7bb59de
Burlesque1 <123176000@qq.com>
2018-04-03 22:50:11 -0400

    add favicon and patial socket

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 // for test purpose
 require('./config/config')
 
-
 const express = require('express');
 const hbs = require('hbs');
 const bodyParser = require('body-parser');
 const moment = require('moment');
 const _ = require('lodash');
 const validator = require('validator');
+const path = require('path');
+const favicon = require('serve-favicon');
 
 var {mongoose} = require('./db/mongoose');
 var {authenticate} = require('./middleware/authenticate');
@@ -16,6 +17,13 @@ var {User} = require('./models/user');
 const port = process.env.PORT;
 
 var app = express();
+var server = require ('http').Server(app);
+var io =  require('socket.io')(server);
+io.on('connection', (socket) => {
+    console.log('New user connected');
+});
+
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials'); // need full path here
@@ -27,9 +35,10 @@ hbs.registerHelper('screamIt', (text) => {
 });
 
 // static serves static files and uses complete path
-// public set index.html as '/' by default
-app.use(express.static(__dirname + '/public'))
+// public uses index.html prior to get '/'
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')))
 
 // app.use((req, res, next) => {
 //     res.render('maintenance.hbs');
@@ -41,6 +50,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 //     console.log(`${now} ${req.method} ${req.url}`);
 //     next();
 // });
+
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
@@ -89,7 +99,13 @@ app.post('/room', async (req, res) => {
         res.status(400).redirect('/');
     }
 })
-app.listen(port, () => {
+
+
+
+
+
+
+server.listen(port, () => {
     console.log(`Started up at port ${port} at ${moment()}`);
 })
 
